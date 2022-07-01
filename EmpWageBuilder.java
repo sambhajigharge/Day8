cc2affd56d46cab791fafb4f7808b54b4a42b54e
sushant <sgharge299@gmail.com>
2022-07-01 17:46:26 +0530

    [sushant]checked the condition present or absent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,6 +2,12 @@ package Day8;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Emp Wage Computation");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
