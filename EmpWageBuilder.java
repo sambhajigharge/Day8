7f1ed84b4e0175598d3df4ecbb75f7463f5fe4f8
sushant <sgharge299@gmail.com>
2022-07-01 17:52:35 +0530

    [sushant]calculated daily emp wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,11 +3,17 @@ package Day8;
 public class EmpWageBuilder {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
 
-        if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+        }
         else
-            System.out.println("Employee is Absent");
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
