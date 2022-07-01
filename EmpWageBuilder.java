a0d79797781ee3cd193b1c27d0c0dbc337bd9ce0
sushant <sgharge299@gmail.com>
2022-07-01 18:03:35 +0530

    [sushant]calculated monthly emp wage for part time and full time emp

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,32 @@
 package Day8;
 
 public class EmpWageBuilder {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+
     public static void main(String[] args) {
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
 
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0;
-        int empWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        } else if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        } else
-            empHrs = 0;
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("Emp Wage: " + empWage);
+        }
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
